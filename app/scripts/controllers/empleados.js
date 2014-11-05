bd83ddd5b523263b7897da2e8c1ab66cb33647a0
David Domenech <ddomenech@gmail.com>
2014-11-05 18:51:29 +0100

    incluir id de empleado

--- app/scripts/controllers/empleados.js
+++ app/scripts/controllers/empleados.js
@@ -18,50 +18,15 @@ angular.module('tareasAsoinApp')
     var refEmpleados = new Firebase("https://tareasddp.firebaseio.com/empleados")
     var syncFireEmp = $firebase(refEmpleados);
     $scope.empleados = syncFireEmp.$asArray();
-    /*[
-	{
-		id: 1, nombre: 'David', apellidos: 'Domenech Pascual', fechainicio: new Date(), 
-		salario: 17000, telefono: 9633344343, recurso: 'Programador Junior', activo: true
-	},
-	{
-		id: 2, nombre: 'Carlos', apellidos: 'Garcia Paya', fechainicio: new Date(), 
-		salario: 27000, telefono: 9533777343, recurso: 'Consultor', activo: true
-	},
-	{
-		id: 3, nombre: 'Jose Manuel', apellidos: 'ñuñez Esteso', fechainicio: new Date(), 
-		salario: 37000, telefono: 9733344343, recurso: 'Comercial', activo: true
-	},
-	{
-		id: 4, nombre: 'Paco', apellidos: ' Pascual', fechainicio: new Date(),
-		salario: 15320, telefono: 92334441343, recurso: 'Programador Senior', activo: true
-	},
-	{
-		id: 5, nombre: 'Sergio', apellidos: 'Domenech Pascual', fechainicio: new Date(),
-		salario: 18800, telefono: 36334444343, recurso: 'Analista', activo: true
-	},
-	{
-		id: 6, nombre: 'Agustin', apellidos: 'Domenech Pascual', fechainicio: new Date(),
-		salario: 17000, telefono: 9633344343, recurso: 'Tecnito', activo: true
-	},
-	{
-		id: 7, nombre: 'Marta', apellidos: 'Lopez ', fechainicio: new Date(),
-		salario: 17000, telefono: 9633344343, recurso: 'Gerente', activo: false
-	},
-	{
-		id: 8, nombre: 'Rafael', apellidos: 'Villarroya Tormo', fechainicio: new Date(),
-		salario: 17000, telefono: 9633344343, recurso: 'Consultor Jefe', activo: false
-	},
-	{	
-		id: 9, nombre: 'Guillermito', apellidos: 'Puertas y Ventanas', fechainicio: new Date(),
-		salario: 17000, telefono: 9633344343, recurso: 'Jefe Programadores', activo: true
-	 }
-	];*/
+    
 	$scope.limpiar = function(){
+		var nextid = 0;
+		nextid = $scope.empleados.length;
 		$scope.empleado = {
-		id: 0, 
+		id: nextid+1, 
 		nombre: '', 
 		apellidos: '', 
-		fecha: new Date(),
+		fecha: null,	
 		salario: 0, 
 		telefono: 0,
 		tipo: '',
@@ -77,6 +42,7 @@ angular.module('tareasAsoinApp')
 	}
 
 	$scope.abrir= function(id){
+		
 		var empleadoSalvado = $modal.open({
 			templateUrl: "Empleado-form.html",
 			controller: "EmpleadoSaveCntrl", 
@@ -104,9 +70,16 @@ angular.module('tareasAsoinApp')
   	 function ($scope, $modalInstance, empleado) {
   	 	$scope.empleado = empleado;
   		
-  		$scope.fecha = {
-  			dateFormat: 'dd-mm-yy'
+  		$scope.dateOptions = {
+  			dateFormat: 'dd-MM-yyyy',
+  			startingDay: 1
   		}
+  		$scope.open = function($event) {
+    		$event.preventDefault();
+	    	$event.stopPropagation();
+
+    		$scope.opened = true;
+  		};
   		$scope.ok = function() {
   			$modalInstance.close($scope.empleado);
   		}
